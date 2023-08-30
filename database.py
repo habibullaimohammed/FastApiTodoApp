6932beab1448d89b28f2c323bcf2a73cbf55dd03
Habibullahi Mohammed <habibullaimohammed@gmail.com>
2023-08-30 03:23:17 +0100

    added cloud db

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://todoappfastapi_user:YnfdxdTtsSdvmbvSYRMzEqvYoDP2pO9c@dpg-cjnac00cfp5c7385c7f0-a.oregon-postgres.render.com/todoappfastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://todoappfastapi_user:YnfdxdTtsSdvmbvSYRMzEqvYoDP2pO9c@dpg-cjnac00cfp5c7385c7f0-a/todoappfastapi"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
